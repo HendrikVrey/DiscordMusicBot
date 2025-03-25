9304bff7ecd4ba63bb56f386078ad6d58b2d6f81
Hendrik Vrey <vreyhendrik787@gmail.com>
2025-03-26 00:55:40 +0200

    Added better error logging to the application

--- DiscordMusicBot/Program.cs
+++ DiscordMusicBot/Program.cs
@@ -16,63 +16,72 @@ namespace DiscordMusicBot
 
         static async Task Main(string[] args)
         {
-            var jsonReader = new JSONReader();
-            await jsonReader.ReadJSON().ConfigureAwait(false);
-
-            var discordConfig = new DiscordConfiguration
+            try
             {
-                Intents = DiscordIntents.All,
-                Token = jsonReader.token,
-                TokenType = TokenType.Bot,
-                AutoReconnect = true
-            };
+                var jsonReader = new JSONReader();
+                await jsonReader.ReadJSON().ConfigureAwait(false);
 
-            Client = new DiscordClient(discordConfig);
+                var discordConfig = new DiscordConfiguration
+                {
+                    Intents = DiscordIntents.All,
+                    Token = jsonReader.token,
+                    TokenType = TokenType.Bot,
+                    AutoReconnect = true
+                };
 
-            Client.Ready += Client_Ready;
+                Client = new DiscordClient(discordConfig);
+                Client.Ready += Client_Ready;
 
-            var commandsConfig = new CommandsNextConfiguration
+                var commandsConfig = new CommandsNextConfiguration
+                {
+                    StringPrefixes = new string[] { jsonReader.prefix },
+                    EnableMentionPrefix = true,
+                    EnableDms = true,
+                    EnableDefaultHelp = false,
+                };
+
+                Client.UseInteractivity(new InteractivityConfiguration
+                {
+                    Timeout = TimeSpan.FromMinutes(2)
+                });
+
+                Commands = Client.UseCommandsNext(commandsConfig);
+
+                Commands.RegisterCommands<MusicCommands>();
+                Commands.RegisterCommands<UtilityCommands>();
+
+                var endPoint = new ConnectionEndpoint
+                {
+                    Hostname = "lava-v3.ajieblogs.eu.org",
+                    Port = 443,
+                    Secured = true,
+                };
+
+                var lavalinkConfig = new LavalinkConfiguration
+                {
+                    Password = "https://dsc.gg/ajidevserver",
+                    RestEndpoint = endPoint,
+                    SocketEndpoint = endPoint
+                };
+
+                var lavaLink = Client.UseLavalink();
+
+                await Client.ConnectAsync().ConfigureAwait(false);
+                await lavaLink.ConnectAsync(lavalinkConfig).ConfigureAwait(false);
+
+                Console.WriteLine("Bot is connected and running.");
+                await Task.Delay(-1).ConfigureAwait(false);
+            }
+            catch (Exception ex)
             {
-                StringPrefixes = new string[] { jsonReader.prefix },
-                EnableMentionPrefix = true,
-                EnableDms = true,
-                EnableDefaultHelp = false,
-            };
-
-            Client.UseInteractivity(new InteractivityConfiguration
-            {
-                Timeout = TimeSpan.FromMinutes(2)
-            });
-
-            Commands = Client.UseCommandsNext(commandsConfig);
-
-            Commands.RegisterCommands<MusicCommands>();
-            Commands.RegisterCommands<UtilityCommands>();
-
-            var endPoint = new ConnectionEndpoint
-            {
-                Hostname = "lava-v3.ajieblogs.eu.org",
-                Port = 443,
-                Secured = true,
-            };
-
-            var LavaLinkConfig = new LavalinkConfiguration
-            {
-                Password = "https://dsc.gg/ajidevserver",
-                RestEndpoint = endPoint,
-                SocketEndpoint = endPoint
-            };
-
-            var LavaLink = Client.UseLavalink();
-
-            await Client.ConnectAsync().ConfigureAwait(false);
-            await LavaLink.ConnectAsync(LavaLinkConfig).ConfigureAwait(false);
-
-            await Task.Delay(-1).ConfigureAwait(false);
+                Console.WriteLine($"Fatal error during startup: {ex.Message}");
+                Console.WriteLine(ex.StackTrace);
+            }
         }
 
         private static Task Client_Ready(DiscordClient sender, DSharpPlus.EventArgs.ReadyEventArgs args)
         {
+            Console.WriteLine("Bot is ready!");
             return Task.CompletedTask;
         }
     }
